617ddadf91a98a9fa25d43f1a80404b2204b927e
Haolg1812 <giahaobanh2004@gmail.com>
2024-11-08 10:21:02 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+const PORT = process.env.PORT || 3001;
+const dbURL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.rohbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const app = express();
+
+app.listen(PORT,() => {
+console.log(`Server is starting at http://localhost:${PORT}`)
+})
